8c232c3f1a4105beae76b7f647ebaf6b84efcfdf
javython999 <infinity2544@gmail.com>
2025-06-25 21:30:29 +0900

    Initial commit

---
+++ src/main/java/com/errday/artillery/ArtilleryApplication.java
@@ -0,0 +1,13 @@
+package com.errday.artillery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtilleryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtilleryApplication.class, args);
+    }
+
+}
